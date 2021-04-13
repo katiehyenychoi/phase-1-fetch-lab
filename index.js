af8dbc906da9f9b00099278b30b4726b9626fb5b
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-13 16:36:42 -0400

    commit tree 443520245a0ef608c07065f6948142d220586645

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(res => res.json())
+    .then((booksArr) => {
+      renderBooks(booksArr)
+    })
+};
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +16,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
